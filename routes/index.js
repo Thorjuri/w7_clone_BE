39678da972c6681e27d94f6188e4943c8fcbf040
Thorjuri <sjl1992@naver.com>
2022-10-28 16:14:40 +0900

    10/28 서주리 routes index.js 수정

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const postsRouter = require('./posts.js');
+const usersRouter = require('./users.js');
+const bucketsRouter = require('./buckets.js');
+const likesRouter = require('./likes.js');
+
+
+//전역 미들웨어
+
+router.use("/posts", postsRouter);
+router.use("/users", usersRouter);
+router.use("/buckets", bucketsRouter);
+router.use("/likes", likesRouter);
+
+
+module.exports = router;
