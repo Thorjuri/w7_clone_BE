96e984945843aac22f6b61fab9e18a152bfe9f43
Thorjuri <sjl1992@naver.com>
2022-10-29 16:14:00 +0900

    websocket 다대다 익명 채팅 구현 완료

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('./models');
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,6 +26,32 @@ app.use("/", Router);
 app.use('/' ,errorHandlerMiddleware); // 에러 핸들러
 
 
-app.listen(port, () => {
-    console.log(port, '포트로 서버가 열렸어요!');
+// WebSocket - 익명 다대다 채팅
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket)=>{
+    socket.on('request_message', (msg) => {
+        // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
+        io.emit('response_message', msg);
+    });
+
+    socket.on('disconnect', async () => {
+        console.log('user disconnected');
+    });
+});
+
+
+// TEST CODE GOES HERE
+(async function(){
+})();
+
+
+http.listen(port, () => {
+    console.log(`${port}번 포트로 서버 실행`);
+});
+
+// app.listen(port, () => {
+//     console.log(port, '포트로 서버가 열렸어요!');
+// });
