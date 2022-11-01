1372809383843e3396a5c76aa7de74ddecfbc55c
Thorjuri <sjl1992@naver.com>
2022-11-01 11:24:28 +0900

    11/1 서주리 웹소켓 1:1 채팅, 연결 소켓 하드코딩, 로컬스토리지에 이용자 닉네임 저장 및 채팅 시 할당

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3005;
+const port = 4000;
 const Router = require('./routes/index');
 const errorHandlerMiddleware = require('./middlewares/error_handler_middleware');
 const cookieParser = require('cookie-parser');
@@ -28,29 +28,44 @@ app.use('/', errorHandlerMiddleware); // 에러 핸들러
 
 
 
+
+// 로그인 클라이언트연결 테스트
+app.get('/test/login', (req,res)=>{
+    res.sendFile(__dirname + '/login.html');
+})
+
 // WebSocket - 실시간 채팅 
-app.get('/chat', auth_middleware, async(req, res) => {
-    const loginId = res.locals.user.loginId  //현재 로그인된 유저의 loginId 도출
-    res.header('userId', loginId)  //응답 헤더에 넣어줌
+app.get('/chat1', async(req, res) => {
+    // const loginId = res.locals.user.loginId  //현재 로그인된 유저의 loginId 도출
+    // res.header('userId', loginId)  //응답 헤더에 넣어줌
     res.sendFile(__dirname + '/index.html');
 }); 
 
+app.get('/chat2', async(req, res)=> {
+    res.sendFile(__dirname + '/room2.html')
+})
+
+app.get('/chat3', async(req, res)=> {
+    res.sendFile(__dirname + '/room3.html')
+})
+
+
 
 io.on('connection', (socket)=>{
-    socket.on('request_message', (msg) => {
+    socket.on('room2', (msg) => { 
         // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
-        io.emit('response_message', msg);
-    });
+        io.emit('room_all', msg);
+    })
+
+        socket.on('room3', (msg) => { 
+            // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
+            io.emit('room_all', msg); 
+        });
 
     socket.on('disconnect', async () => {
         console.log('user disconnected');
     });
 });
-
-
-// TEST CODE GOES HERE
-(async function(){
-})();
 
 
 http.listen(port, () => {
