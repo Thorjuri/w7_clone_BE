5373b4d5c41edd54bf9c2f47e83a3f76bfb3c63b
Thorjuri <sjl1992@naver.com>
2022-11-01 12:16:18 +0900

    11/1 서주리 multer 이용 사진업로드 기능 구현 중

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const errorHandlerMiddleware = require('./middlewares/error_handler_middleware')
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('./models');
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +21,38 @@ app.use(
 );
 
 app.options('*', cors());
+
+
+
+try{
+    fs.readdirSync('uploads')
+} catch (error){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads')
+}
+
+const upload = multer({
+    storage : multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/');
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits : { fileSize: 7 * 2048 * 2048 }
+})
+
+app.get('/post', (req, res)=> {
+    res.sendFile('/post.html')
+})
+
+app.post('/post/upload', upload.single('image'), (req, res)=> {
+    console.log(req.file, req.body);
+    res.send('ok')
+})
+
 
 app.use('/', Router);
 app.use('/', errorHandlerMiddleware); // 에러 핸들러
