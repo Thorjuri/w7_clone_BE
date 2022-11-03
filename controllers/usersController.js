9161e6f45bccc2e9997be9c59e128d8721249f11
tastekim <tastekim@kakao.com>
2022-11-03 16:59:32 +0900

    checkId 유효성 검사 추가

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,10 +9,9 @@ class UsersController {
         if (req.headers.authorization) {
             const err = new Error(`UserController Error`);
             err.status = 401;
-            err.message = '이미 로그인이 되어있습니다.'
+            err.message = '이미 로그인이 되어있습니다.';
             throw err;
-        }
-         else {
+        } else {
             const { loginId, password } = req.body;
 
             await this.UserService.signUpUser(loginId, password);
@@ -22,6 +21,13 @@ class UsersController {
 
     checkId = async (req, res) => {
         const { loginId } = req.body;
+        const regexp = new RegExp(/^[a-z]+[a-z0-9]{5,19}$/g);
+        if (!regexp.test(loginId)) {
+            const err = new Error(`UserController Error`);
+            err.status = 400;
+            err.message = '아이디 양식을 맞춰주세요.';
+            throw err;
+        }
 
         await this.UserService.checkId(loginId);
         res.status(200).send('해당 아이디는 사용 가능합니다');
@@ -31,7 +37,7 @@ class UsersController {
         if (req.headers.authorization) {
             const err = new Error(`UserController Error`);
             err.status = 401;
-            err.message = '이미 로그인이 되어있습니다.'
+            err.message = '이미 로그인이 되어있습니다.';
             throw err;
         } else {
             const { loginId, password } = req.body;
@@ -51,7 +57,6 @@ class UsersController {
         res.status(200).json(likeslist);
     };
 
-
     userBuckets = async (req, res) => {
         const { userId } = res.locals.user;
         try {
@@ -61,7 +66,6 @@ class UsersController {
         } catch (err) {
             next(err);
         }
-
     };
 }
 
